498b68670d0f3d973e4abd26b87153557886da5f
Oleksandr Hochu <gochualex@outlook.com>
2026-06-08 16:41:17 +0100

    Creates spotify resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { SpotifyModule } from './spotify/spotify.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, expandVariables: true }), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true, expandVariables: true }), PrismaModule, SpotifyModule],
 })
 export class AppModule {}
