540c0ce3a5e469f85e0c78afbb5b2742f6fe7020
Oleksandr Hochu <gochualex@outlook.com>
2026-06-08 15:07:35 +0100

    Applies initial e2e test fom artists

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArtistModule } from './artist/artist.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -9,5 +11,7 @@ import { ArtistModule } from './artist/artist.module';
     PrismaModule,
     ArtistModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
