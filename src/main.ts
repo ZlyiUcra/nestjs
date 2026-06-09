f5cbd3cd120d0c312e1f4d132b05fe99a201eec6
Oleksandr Hochu <gochualex@outlook.com>
2026-06-09 18:56:58 +0100

    Adds initial configuration for versioning

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
@@ -9,6 +9,10 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
   //app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 3000);
