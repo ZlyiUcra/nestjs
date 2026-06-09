b3f21eaeecbd2f0528d5ac74ad2d1b89adc828ad
Oleksandr Hochu <gochualex@outlook.com>
2026-06-09 18:33:04 +0100

    Configbres main.ts file completle for working cors

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
   //app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3000'],
+    origin: configService.getOrThrow<string>('ALLOWED_ORIGINs').split(','),
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    exposedHeaders: ['Set-Cookie', 'Content-Disposition'],
+    allowedHeadsers: ['Authorization', 'X-Api-Key'], //'*'
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(configService.getOrThrow<number>('PORT'));
 }
 bootstrap();
